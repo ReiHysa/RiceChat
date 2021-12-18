56bf563984adf662e86f09f260365e29ec1789c9
Reisli Hysa <reislihysa@Isabellas-iMac.local>
2021-12-18 00:25:13 +0000

    please

--- ricechat/views.py
+++ ricechat/views.py
@@ -1,5 +1,13 @@
+# from django.shortcuts import render
+
+
+# def index(request):
+#     return render(request, 'build/index.html')
+
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
+# Create your views here.
 
 
-def index(request):
-    return render(request, 'build/index.html')
+class LandingView(TemplateView):
+    template_name = "base/index.html"
