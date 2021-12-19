76fbb4f1a128891da24941b6a1d7dc7c853bea75
Reisli Hysa <reislihysa@Isabellas-iMac.local>
2021-12-19 20:47:25 +0000

    er

--- ricechat/settings.py
+++ ricechat/settings.py
@@ -61,7 +61,9 @@ MIDDLEWARE = [
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': os.path.join(BASE_DIR, 'client'),
+        'DIRS': [os.path.join(BASE_DIR, 'client')
+                 ]  # Look, we have added the root folder of frontend here
+        ,
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
