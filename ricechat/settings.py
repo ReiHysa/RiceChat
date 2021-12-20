adfccd2d7e2d3bc8a8489509c3789d072b275f11
Reisli Hysa <reislihysa@Isabellas-iMac.local>
2021-12-20 22:50:04 +0000

    ess

--- ricechat/settings.py
+++ ricechat/settings.py
@@ -34,7 +34,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -48,7 +47,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -144,6 +142,5 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
     ],
 }
-CORS_ORIGIN_ALLOW_ALL = True
 
 django_on_heroku.settings(locals())
