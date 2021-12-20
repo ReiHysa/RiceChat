3868d6b7fd90168c930f1f5063a696bc576f6f2f
Reisli Hysa <reislihysa@Isabellas-iMac.local>
2021-12-20 23:41:41 +0000

    add

--- ricechat/settings.py
+++ ricechat/settings.py
@@ -147,8 +147,6 @@ REST_FRAMEWORK = {
 # CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOWED_ORIGINS = [
     "https://rice-chat.herokuapp.com"
-    "http://localhost:8080",
-    "http://127.0.0.1:9000",
 ]
 
 django_on_heroku.settings(locals())
