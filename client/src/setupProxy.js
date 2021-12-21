d43a5a66c1309b927f13f33e19d4e934d54f9c48
Reisli Hysa <reislihysa@Isabellas-iMac.local>
2021-12-21 00:29:55 +0000

    work

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     createProxyMiddleware(
       "/api",
-      { target: "https://rice-chat.herokuapp.com" } // ! Update this line if your API is accessable on a port other than 8000, eg 4000
+      { target: "http://localhost:8000" } // ! Update this line if your API is accessable on a port other than 8000, eg 4000
     )
   );
 };
